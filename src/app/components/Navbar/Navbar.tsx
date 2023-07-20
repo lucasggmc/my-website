f8eaccc9a88208828dc139dac842b93ce8a50529
lucasggmc <lucasggmc@gmail.com>
2023-07-19 23:01:12 -0300

    initial next 13 setup with TypeScript and styled components

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import * as S from './styled';
+
+const Navbar = () => {
+  return (
+    <S.NavContainer>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Contact</li>
+      </ul>
+    </S.NavContainer>
+  );
+};
+
+export { Navbar };
