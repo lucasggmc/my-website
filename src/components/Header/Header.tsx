db9f4103bbaf140b7dfb284f46bb70babaf573bf
lucasggmc <lucasggmc@gmail.com>
2023-08-27 23:08:01 -0300

    added docker file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <S.ContentContainer>
         <main>
           <h1>
-            Hi! I'm <span>Lucas Carneiro</span>
+            Hi! I am <span>Lucas Carneiro</span>
           </h1>
           <h2>
             I am a Full Stack Developer focused on <span>Front End</span>
@@ -20,7 +20,7 @@ const Header = () => {
           <p> I started my career asdasd asdasdasd asdasdas asdas</p>
           <Linkedin size={25} title="linkedin link" />
           {/* <Github size={25} title="github link" /> */}
-          {/* <GithubOutline size={25} title="github link" /> */}
+          <GithubOutline size={25} title="github link" />
         </main>
         <Image src="/site/profile.jpeg" width={200} height={200} alt="Picture of the author" />
       </S.ContentContainer>
