4c0a851e62787bc60826fbacb5f051394417cb4e
lucasggmc <lucasggmc@gmail.com>
2023-11-04 14:37:30 -0300

    fix: small UI adjustments

--- src/components/HamburgerMenuDesktop/HamburgerMenuDesktop.tsx
+++ src/components/HamburgerMenuDesktop/HamburgerMenuDesktop.tsx
@@ -2,25 +2,33 @@
 import Link from 'next/link';
 import * as S from './styled';
 import { useTranslation } from '@/contexts/TranslationContext';
+import { usePathname, useRouter } from 'next/navigation';
 
 const HamburgerMenuDesktop = () => {
   const { siteData } = useTranslation();
+  const pathname = usePathname();
+  const blogPath = '/blog';
+
   return (
     <S.HamburgerMenuDesktopContainer>
       <li>
         <Link href="/">{siteData.navbarList[0]}</Link>
       </li>
-      <li>
-        <Link href="#about">{siteData.navbarList[1]}</Link>
-      </li>
-      <li>
-        <Link href="#experience">{siteData.navbarList[2]}</Link>
-      </li>
-      <li>
-        <Link href="#contact">{siteData.navbarList[3]}</Link>
-      </li>
+      {pathname !== blogPath && (
+        <>
+          <li>
+            <Link href="#about">{siteData.navbarList[1]}</Link>
+          </li>
+          <li>
+            <Link href="#experience">{siteData.navbarList[2]}</Link>
+          </li>
+          <li>
+            <Link href="#contact">{siteData.navbarList[3]}</Link>
+          </li>
+        </>
+      )}
       <S.HighlightItem>
-        <Link href="/blog">{siteData.navbarList[4]}</Link>
+        <Link href={blogPath}>{siteData.navbarList[4]}</Link>
       </S.HighlightItem>
     </S.HamburgerMenuDesktopContainer>
   );
--- src/components/HamburgerMenuMobile/HamburgerMenuMobile.tsx
+++ src/components/HamburgerMenuMobile/HamburgerMenuMobile.tsx
@@ -2,9 +2,13 @@
 import Link from 'next/link';
 import * as S from './styled';
 import { useTranslation } from '@/contexts/TranslationContext';
+import { usePathname } from 'next/navigation';
 
 const HamburgerMenuMobile = () => {
   const { siteData } = useTranslation();
+  const pathname = usePathname();
+  const blogPath = '/blog';
+
   return (
     <S.HamburgerMenuContainer>
       <input type="checkbox" />
@@ -17,15 +21,19 @@ const HamburgerMenuMobile = () => {
         <li>
           <Link href="/">{siteData.navbarList[0]}</Link>
         </li>
-        <li>
-          <Link href="#about">{siteData.navbarList[1]}</Link>
-        </li>
-        <li>
-          <Link href="#experience">{siteData.navbarList[2]}</Link>
-        </li>
-        <li>
-          <Link href="#contact">{siteData.navbarList[3]}</Link>
-        </li>
+        {pathname !== blogPath && (
+          <>
+            <li>
+              <Link href="#about">{siteData.navbarList[1]}</Link>
+            </li>
+            <li>
+              <Link href="#experience">{siteData.navbarList[2]}</Link>
+            </li>
+            <li>
+              <Link href="#contact">{siteData.navbarList[3]}</Link>
+            </li>
+          </>
+        )}
         <S.HighlightItem>
           <Link href="/blog">{siteData.navbarList[4]}</Link>
         </S.HighlightItem>
